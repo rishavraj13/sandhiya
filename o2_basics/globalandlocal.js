aa1ff5cceff5f46ed7ed4a37114fe5646d5af365
Rishav raj <146652774+rishavraj13@users.noreply.github.com>
2024-12-27 15:48:19 +0000

    part 1 basics

---
+++ o2_basics/globalandlocal.js
@@ -0,0 +1,29 @@
+//{} these curly braces are called SCOPES... almost har programming language isi ko acope bolte hai
+/*objects mei curly braces ka matlab hai for object declarestion 
+but in functions or with if-else kei saath aaye tab waha isko bolte hai SCOPES 
+
+IF kei andar jo bhi likahye ga woh hai block scope and bahar mei jo hoga woh hai global scope 
+
+SCOPE kei andar jo value likhte hai woh VALUE bahar nahi jani chahiye
+
+
+
+
+
+*/
+
+//example for block and global scope
+
+let a = 25; //its a global 
+
+if(true){ //yeh hai block scope
+    let a = 456;
+    console.log(a);
+    
+};
+
+console.log(a);
+
+//********************************************************* *
+
+
