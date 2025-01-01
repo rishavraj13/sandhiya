5cb522a2e44071ad772f751feddb6628812da2d1
Rishav raj <146652774+rishavraj13@users.noreply.github.com>
2025-01-01 17:33:36 +0000

    notes

---
+++ 03_basic/controlflow.js
@@ -0,0 +1,39 @@
+// IF statement
+/*agar IF staement kei andar FALSE hoga tho uske andar ka code execute nahi hoga
+"===" yeh jo triple equal hai woh type bhi check karta hai (dataType)
+///////////////////////////////////////////////////////////////////////////////////////////////////
+Falsy Values
+A falsy value is a value that is considered false when converted to a Boolean. 
+There are only six falsy values in JavaScript:
+
+false: The Boolean value false.
+0: The number zero.
+-0: Negative zero.
+"" (empty string): A string with no characters.
+null: The absence of any value.
+undefined: A variable that has been declared but not assigned a value.
+NaN: Not-a-Number.
+
+*******************************************************************************8
+
+Truthy Values
+A truthy value is any value that is not falsy. 
+Truthy values are considered true when evaluated in a Boolean context.
+
+Examples of Truthy Values:
+Any non-zero number: 1, -5, 3.14
+Any non-empty string: "hello", " "
+Objects and arrays: {}, []
+true (the Boolean value)
+Special values like Infinity and -Infinity
+
+
+
+
+
+***************************************************888
+
+
+
+
+*/
