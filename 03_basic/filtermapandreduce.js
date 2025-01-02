d436c73425a1fdcf7d575cd2a0b2434bbac3f265
Rishav raj <146652774+rishavraj13@users.noreply.github.com>
2025-01-02 18:35:49 +0000

    .

---
+++ 03_basic/filtermapandreduce.js
@@ -0,0 +1,7 @@
+ //For Each jo hai woh koi bhi value return nahi karwata hai 
+//scope start karegay tho RETURN use karna hi parega
+
+ const mynums = [1,5,7,5,43,78]
+ const newnums = mynums.filter((num) => num > 4)
+ console.log(newnums);
+ 
