85a09eccb5736aecc40b44ea8f8a528dfc0ee5fe
Rishav raj <146652774+rishavraj13@users.noreply.github.com>
2025-01-02 17:23:57 +0000

    part 2

---
+++ 03_basic/highorder2.js
@@ -0,0 +1,43 @@
+// const coding = ["js", "ks", "ms"];
+
+// coding.forEach(function (val) {
+//     console.log(val);
+
+// });
+
+// coding.forEach((item) => {
+//     console.log(item);
+
+// });
+
+// function printme(item){
+//     console.log(item);
+
+// }
+
+// coding.forEach(printme)
+
+const mycode = [
+    {
+        langname: "javascript",
+        filename: "js"
+
+    },
+
+    {
+        langname: "python",
+        filename: "py"
+    },
+
+    {
+        langname: "java",
+        filename: "java"
+    },
+
+]
+
+mycode.forEach( (item) => {
+    console.log(item.langname);
+    
+})
+
